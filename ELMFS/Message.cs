5b5040c9a3b020a080bd41767fdd2e8cda34deb6
Fletcher40340889 <40340889@live.napier.ac.uk>
2020-06-30 00:47:10 +0100

    Comment for Message.cs

--- ELMFS/Message.cs
+++ ELMFS/Message.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Author: Fletcher Thomas Moore
+//Description: Handles the message as a whole, writing the inputs to a JSON format file. Connected to MainWindow.xaml.cs
+//Start Date: 29/06/2020
+//End Date: 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
