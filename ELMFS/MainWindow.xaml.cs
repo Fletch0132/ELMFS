c572ba0c16dd1cc19dce56a89262d3432e187603
Fletcher40340889 <40340889@live.napier.ac.uk>
2020-06-30 16:06:16 +0100

    Connection made to TypeSMS, TypeEmail, and TypeTweet

--- ELMFS/MainWindow.xaml.cs
+++ ELMFS/MainWindow.xaml.cs
@@ -3,19 +3,11 @@
 //Start Date: 29/06/2020
 //End Date: 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ELMFS.TypeEmail;
+using ELMFS.TypeSms;
+using ELMFS.TypeTweet;
 
 namespace ELMFS
 {
