04442b0dba4ec236f6b8f9920db456f18ab4c3b4
Fletcher40340889 <40340889@live.napier.ac.uk>
2020-07-11 19:48:16 +0100

    SIRList GUI

---
+++ ELMFS/Displays/Quarantine.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ELMFS.Displays
+{
+    /// <summary>
+    /// Interaction logic for Quarantine.xaml
+    /// </summary>
+    public partial class Quarantine : Window
+    {
+        public Quarantine()
+        {
+            InitializeComponent();
+        }
+    }
+}
